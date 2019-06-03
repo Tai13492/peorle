8a339574dd524d5d4d8063005deb08ea4941a6d1
Tai <friendship045@gmail.com>
2019-06-03 21:26:35 +0700

    feat: add meta data

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,8 @@ import { Helmet } from "react-helmet"
 const Header = () => (
   <Helmet>
     <meta charset="utf-8" />
-    <title> Hello World </title>
+    <title> Peorle </title>
+    <meta name="description" content="ผลิตภัณฑ์เสริมอาหารพีออร์เล่" />
     <link
       href="https://fonts.googleapis.com/css?family=Kanit:200,300,400,500&display=swap"
       rel="stylesheet"
