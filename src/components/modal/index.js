68fb05e0f2ba4289a1080e0ff8ec1e7c097b1c1b
Tai <friendship045@gmail.com>
2019-06-17 00:06:16 +0700

    fix maxHeight and maxWidth to 500px and 500px

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -82,7 +82,7 @@ const Image = () => (
     render={data => (
       <Img
         fluid={data["work_with_us"].childImageSharp.fluid}
-        style={{ maxHeight: 900 }}
+        style={{ maxWidth: 500, maxHeight: 500 }}
         // style={{ marginBottom: 20 }}
       />
     )}
